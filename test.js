b40fa3d0dcdd1029a11ac2f812fccb828a7f2909
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-01-23 17:14:21 -0300

    actualizancion menor.

--- test.js
+++ test.js
@@ -1,14 +1,18 @@
 var AWS = require('aws-sdk');
 
+/*
+ * https://github.com/aws/aws-sdk-js/blob/master/doc-src/guide/node-examples.md
+ */
+
 AWS.config.update({
   accessKeyId: 'aFSoSE4BY2yHGf3oDQXz',
   secretAccessKey: 'WDjIV0nKThCjKmWFbfrsWqwLVrtB4lqgSLGt-evD'
 });
 
-var host = 'test-tagger-app.objects.dreamhost.com';
-var bucket = 'test-tagger-app.objects.dreamhost.com';
+//var host = 'test-tagger-app.objects.dreamhost.com';
+var bucket = '12312test-tagger-app.objects.dreamhost.com';
 
-var ep = new AWS.Endpoint(host);
+var ep = new AWS.Endpoint('objects.dreamhost.com');
 var s3 = new AWS.S3({endpoint: ep});
 //s3.service.endpoint.hostname == host;
 
@@ -20,9 +24,22 @@ var s3 = new AWS.S3({endpoint: ep});
 
       if (err)
           console.log(err)
-      else
+      else {
           console.log("Successfully uploaded data to myBucket/myKey");
+          console.log(data);
+        }
 
    });
 
+});
+
+
+s3.listBuckets(function(err, data) {
+  if (err) { console.log("Error:", err); }
+  else {
+    for (var index in data.Buckets) {
+      var bucket = data.Buckets[index];
+      console.log("Bucket test: ", bucket.Name, ' : ', bucket.CreationDate);
+    }
+  }
 });
